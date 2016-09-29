17d1df8aa53890c148900308ab2a0be71f8dedf6
陈恺垣 <chenkaiyuan1993@gmail.com>
2016-09-29 19:06:17 +0800

    增加极验前端

--- php/resources/views/geetest.blade.php
+++ php/resources/views/geetest.blade.php
@@ -12,9 +12,33 @@
 <a href="../" class=>返回</a>
 <h1>极验</h1>
 <hr>
-
+<div id="captcha"></div>
 
 <script src="/asset/js/jquery-1.12.3.min.js"></script>
-<script src="https://static.geetest.com/static/tools/gt.js"></script>
+<script src="//static.geetest.com/static/tools/gt.js"></script>
+<script>
+
+    var handler = function (captchaObj) {
+        // 将验证码加到id为captcha的元素里
+        captchaObj.appendTo("#captcha");
+    };
+    $.ajax({
+        // 获取id，challenge，success（是否启用failback）
+        type: "GET",
+        url: "/geetest/preProcess?rand=" + Math.round(Math.random() * 100),
+        dataType: "json", // 使用jsonp格式
+        success: function (data) {
+            // 使用initGeetest接口
+            // 参数1：配置参数，与创建Geetest实例时接受的参数一致
+            // 参数2：回调，回调的第一个参数验证码对象，之后可以使用它做appendTo之类的事件
+            initGeetest({
+                gt: data.gt,
+                challenge: data.challenge,
+                product: "float", // 产品形式
+                offline: !data.success
+            }, handler);
+        }
+    });
+</script>
 </body>
 </html>
