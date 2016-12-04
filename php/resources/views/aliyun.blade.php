a050e472f983cfd549b6c67cdc531888bb473711
陈恺垣 <chenkaiyuan1993@gmail.com>
2016-12-04 17:16:12 +0800

    php make piwik better

--- php/resources/views/aliyun.blade.php
+++ php/resources/views/aliyun.blade.php
@@ -70,25 +70,7 @@
         }
     });
 </script>
-<!-- Piwik -->
-<script type="text/javascript">
-    var _paq = _paq || [];
-    _paq.push(["setDomains", ["*.just4fun.chenky.com"]]);
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function () {
-        var u = "//piwik.chenky.com/";
-        _paq.push(['setTrackerUrl', u + 'piwik.php']);
-        _paq.push(['setSiteId', '2']);
-        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-        g.type = 'text/javascript';
-        g.async = true;
-        g.defer = true;
-        g.src = u + 'piwik.js';
-        s.parentNode.insertBefore(g, s);
-    })();
-</script>
-<noscript><p><img src="//piwik.chenky.com/piwik.php?idsite=2" style="border:0;" alt=""/></p></noscript>
-<!-- End Piwik Code -->
+
+@include('piwik')
 </body>
 </html>
--- php/resources/views/touclick.blade.php
+++ php/resources/views/touclick.blade.php
@@ -18,25 +18,6 @@
     <li><a href="{{ env('URL_PREFIX') }}/touclick/3" target="_blank">方式3 图标选择型</a></li>
 </ul>
 
-<!-- Piwik -->
-<script type="text/javascript">
-    var _paq = _paq || [];
-    _paq.push(["setDomains", ["*.just4fun.chenky.com"]]);
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function () {
-        var u = "//piwik.chenky.com/";
-        _paq.push(['setTrackerUrl', u + 'piwik.php']);
-        _paq.push(['setSiteId', '2']);
-        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-        g.type = 'text/javascript';
-        g.async = true;
-        g.defer = true;
-        g.src = u + 'piwik.js';
-        s.parentNode.insertBefore(g, s);
-    })();
-</script>
-<noscript><p><img src="//piwik.chenky.com/piwik.php?idsite=2" style="border:0;" alt=""/></p></noscript>
-<!-- End Piwik Code -->
+@include('piwik')
 </body>
 </html>
